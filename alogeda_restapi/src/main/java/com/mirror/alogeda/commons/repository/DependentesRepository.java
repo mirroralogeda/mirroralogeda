4eff3139bb36f52fd61d7a9ca5468fdaaf0834ec
Elenara <elenara.hein@gmail.com>
2018-06-02 11:28:44 -0300

    Cadastro de dependentes

--- alogeda_restapi/src/main/java/com/mirror/alogeda/commons/repository/DependentesRepository.java
+++ alogeda_restapi/src/main/java/com/mirror/alogeda/commons/repository/DependentesRepository.java
@@ -1,14 +1,16 @@
-/*package com.mirror.alogeda.commons.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+package com.mirror.alogeda.commons.repository;
 
 import java.util.List;
 
-import com.mirror.alogeda.commons.model.Cargos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.mirror.alogeda.commons.model.Atestados;
+import com.mirror.alogeda.commons.model.Dependentes;
 
 @Repository
-public interface DependentesRepository extends JpaRepository<Cargos, Integer> {
-    public List<Dependentes> buscarDependentes();
+public interface DependentesRepository extends JpaRepository<Dependentes, Integer> {
+	@Query ("select d from Dependentes d where d.funcionarios.id = ?1")
+	List<Dependentes> findByFuncionario(int funcionario);
 }
-*/
