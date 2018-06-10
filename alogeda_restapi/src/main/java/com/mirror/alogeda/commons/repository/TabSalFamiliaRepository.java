dccfddc13218330a5e34e27f85b9619030f3058e
Ricardo M. Leonarczyk <ricardo.leonarczyk95@gmail.com>
2018-06-10 18:37:42 -0300

    Adicionado cálculo de salário família e imposto de renda

--- alogeda_restapi/src/main/java/com/mirror/alogeda/commons/repository/TabSalFamiliaRepository.java
+++ alogeda_restapi/src/main/java/com/mirror/alogeda/commons/repository/TabSalFamiliaRepository.java
@@ -1,8 +1,10 @@
 package com.mirror.alogeda.commons.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mirror.alogeda.commons.model.TabSalFamilia;
@@ -10,5 +12,6 @@ import com.mirror.alogeda.commons.model.TabSalFamilia;
 
 @Repository
 public interface TabSalFamiliaRepository extends JpaRepository<TabSalFamilia, Integer> {
-	List<TabSalFamilia> findByPerFinalIsNull();
+	@Query("SELECT t FROM TabSalFamilia t WHERE ?1 between t.perInicial and t.perFinal")
+	List<TabSalFamilia> findByVigencia(Date data);
 }
