b173940193f76ab29263787ecae713b6f7defa00
vitorbarth <vitorbackesbarth@gmail.com>
2018-06-14 20:41:15 -0300

    Curriculos - parcial

--- alogeda_restapi/src/main/java/com/mirror/alogeda/commons/service/PessoaService.java
+++ alogeda_restapi/src/main/java/com/mirror/alogeda/commons/service/PessoaService.java
@@ -1,18 +1,26 @@
 package com.mirror.alogeda.commons.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mirror.alogeda.commons.model.Atestados;
 import com.mirror.alogeda.commons.model.Pessoas;
+import com.mirror.alogeda.commons.repository.AtestadosRepository;
 import com.mirror.alogeda.commons.repository.PessoasRepository;
 
 @Service
 public class PessoaService extends CrudService<Pessoas> {
 
+	PessoasRepository pessoaRepository;
 	@Autowired
 	public PessoaService(PessoasRepository pessoaRepository) {
 		super(pessoaRepository);
 	}
+    public Pessoas findByCpf(String cpf){
+    	return pessoaRepository.findByCpfContaining(cpf);
+    }
 
 	
 }
