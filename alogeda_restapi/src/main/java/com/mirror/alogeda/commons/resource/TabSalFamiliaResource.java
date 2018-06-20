5017235dc6b2c49b060377163d1dc781f7f55efa
Ricardo M. Leonarczyk <ricardo.leonarczyk95@gmail.com>
2018-06-20 15:01:53 -0300

    Ajusta datas na tab sal familia.

--- alogeda_restapi/src/main/java/com/mirror/alogeda/commons/resource/TabSalFamiliaResource.java
+++ alogeda_restapi/src/main/java/com/mirror/alogeda/commons/resource/TabSalFamiliaResource.java
@@ -1,5 +1,7 @@
 package com.mirror.alogeda.commons.resource;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -7,12 +9,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.mirror.alogeda.commons.helper.responses.ApiResponse;
 import com.mirror.alogeda.commons.model.TabSalFamilia;
 import com.mirror.alogeda.commons.service.TabSalFamiliaService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 @Path("tabsalfamilia")
@@ -29,18 +31,19 @@ public class TabSalFamiliaResource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("save")
-	public Response save(TabSalFamilia tabSalFamilia) {
-		tabSalFamiliaService.save(tabSalFamilia);
+	public Response save(List<TabSalFamilia> faixas) {
+		for (TabSalFamilia f : faixas)
+			tabSalFamiliaService.save(f);
 		return ApiResponse.ok();
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("delete")
-	public Response delete(TabSalFamilia tabSalFamilia) {
-		tabSalFamiliaService.delete(tabSalFamilia);
+	public Response delete(List<TabSalFamilia> faixas) {
+		for (TabSalFamilia f : faixas)
+			tabSalFamiliaService.delete(f);
 		return ApiResponse.ok();
 	}
 
 }
-
