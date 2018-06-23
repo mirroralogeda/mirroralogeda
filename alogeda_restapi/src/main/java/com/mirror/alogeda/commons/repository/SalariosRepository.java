60356a5e60463a2dced3ff35a22fea211bec09d9
elenara.hein@gmail.com <elenara.hein@gmail.com>
2018-06-23 01:27:42 -0300

    ajustes

--- alogeda_restapi/src/main/java/com/mirror/alogeda/commons/repository/SalariosRepository.java
+++ alogeda_restapi/src/main/java/com/mirror/alogeda/commons/repository/SalariosRepository.java
@@ -3,12 +3,18 @@ package com.mirror.alogeda.commons.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.mirror.alogeda.commons.model.Dependentes;
 import com.mirror.alogeda.commons.model.Salarios;
 
 @Repository
 public interface SalariosRepository extends JpaRepository<Salarios, Integer> {
 	List<Salarios> findByVigFinalIsNull();
 	List<Salarios> findByFuncionariosIdAndVigFinalIsNull(int id);
+	
+	
+	@Query ("select d from Salarios d where d.funcionarios.id = ?1 order by vigInicial")
+	List<Salarios> findByFuncionario(int funcionario);
 }
